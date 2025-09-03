664cce140fab19ab71d97df3d318150b0e89c79f
Melissa <melissa_hartung@teilnehmer.cdemy.de>
2025-09-03 09:41:29 +0200

    add

--- lib/pages/add_rezept.dart
+++ lib/pages/add_rezept.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class AddRezept extends StatelessWidget {
   const AddRezept({super.key});
@@ -10,47 +11,137 @@ class AddRezept extends StatelessWidget {
     final myTextController2 = TextEditingController();
 
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 120, 156, 122),
+      appBar: AppBar(
+        title: Text("Rezepte"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(94, 49, 110, 77),
+        leading: Icon(Icons.menu_rounded),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 20),
+              SizedBox(height: 20),
 
-              // Row with two TextFields
-              Row(
-                children: [
-                  Expanded(child: TextField()),
-                  const SizedBox(width: 10),
-                  Expanded(child: TextField(controller: myTextController)),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Rezeptname",
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              DropdownButtonFormField2<String>(
+                decoration: InputDecoration(
+                  hintText: "Kategorie",
+                  hintStyle: TextStyle(color: Colors.white),
+
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  contentPadding: EdgeInsets.all(15),
+                ),
+                items:
+                    <String>[
+                      'Frühstück',
+                      'Hauptspeise',
+                      'Nachtisch',
+                      'Snack',
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Center(
+                          child: Text(
+                            value,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                onChanged: (String? newValue) {
+                  // Hier müsstest du mit setState den Wert in einer Variable speichern
+                  // (sobald du ein StatefulWidget hast)
+                },
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(255, 71, 121, 82),
+                  ),
+                ),
               ),
 
-              const SizedBox(height: 20),
-              TextField(controller: myTextController1),
-              const SizedBox(height: 20),
-              TextField(controller: myTextController2),
-              const SizedBox(height: 20),
+              SizedBox(height: 20),
+
+              TextField(
+                style: TextStyle(color: Colors.white),
+                controller: myTextController1,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  hintText: "Zutaten",
+                  hintStyle: TextStyle(color: Colors.white),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              TextField(
+                controller: myTextController2,
+                decoration: InputDecoration(
+                  hintText: "Zubereitung",
+                  hintStyle: TextStyle(color: Colors.white),
+
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
 
               Align(
                 alignment: Alignment.centerRight,
                 child: IconButton(
-                  onPressed: () {
-                    print(
-                      "save schritt 1: ${myTextController.text}\n"
-                      "save schritt 2: ${myTextController1.text}\n"
-                      "save schritt 3: ${myTextController2.text}",
-                    );
-                  },
-                  icon: const Icon(
-                    Icons.archive_rounded,
-                    color: Colors.green,
-                    size: 30.0,
-                  ),
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
                 ),
               ),
+              // print(
             ],
+
+            //   "save schritt 1: ${myTextController.text}\n"
+            //   "save schritt 2: ${myTextController1.text}\n"
+            //   "save schritt 3: ${myTextController2.text}",
+            // ),
           ),
         ),
       ),
